56c2b6764017e2a183ba309131aaaf94f16646e3
Valter Cavalcante <valtinholima@gmail.com>
2014-02-28 12:10:43 -0300

    conexao fist

---
+++ application/views/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>WEB - Requisição</title>
+	<link rel="stylesheet" type="text/css" href="http://localhost:8080/conexao/assets/foundation/css/foundation.min.css">
+
+
+</head>
+<body>
+	 <form>
+		<div class="row">
+			<div class="large-4 large-offset-4 columns">
+	        <h4 class="text-center">Solicitação WEB</h4>
+	        <h6 class="text-center">CONEXÃO ENGENHARIA</h6>
+		        <input type="text" placeholder="usuário" required autofocus>
+		        <input type="password" placeholder="senha" required> 
+		        <h6><a href="#" class="text-right"> Esqueci minha senha</a></h6>
+		  	    <button class="success center" type="submit">Entrar</button>
+			</div>
+		</div>
+   </form>
+</body>
+</html>
---
+++ application/views/welcome.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>WEB - Requisição</title>
+	<link rel="stylesheet" type="text/css" href="http://localhost:8080/conexao/assets/css/fundation/css/foundation.min.css">
+
+	
+</head>
+<body>
+	 <form>
+		<div class="row">
+	        <h2 class="form-signin-heading">Login</h2>
+	        <input type="text" class="form-control" placeholder="usuário" required autofocus>
+	        <input type="password" class="form-control" placeholder="senha" required> 
+	        <h6><a href="#" class="text-right"> Esqueci minha senha</a></h6>
+	  	    <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+		</div>
+   </form>
+</body>
+</html>
